e13782255840b7ab8b0c84ef4689387895f626a1
assimbly <r.meester@caesar.nl>
2018-05-18 14:14:14 +0200

    ApplicationProperties

--- src/main/java/org/assimbly/gateway/config/ApplicationProperties.java
+++ src/main/java/org/assimbly/gateway/config/ApplicationProperties.java
@@ -10,5 +10,34 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+	
+    public final Documentation documentation = new Documentation();
+
+    public Documentation getDocumentation() {
+        return documentation;
+    }
+
+    public static class Documentation {
+
+    	private String url;
+    	private String camelUrl;
+    	
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getCamelUrl() {
+            return camelUrl;
+        }
+
+        public void setCamelUrl(String camelUrl) {
+            this.camelUrl = camelUrl;
+        }
+        
+    }
 
 }
