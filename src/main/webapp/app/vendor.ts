01d93835ffc3d3063508fbbd24e2ebe74fd532a9
achraf.adbib <achraf.adbib@hotmail.com>
2020-02-18 22:09:58 +0100

    stop overriding bootstrap

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -1,6 +1,3 @@
 /* after changing this file run 'yarn run webpack:build' */
 /* tslint:disable */
 import '../content/scss/vendor.scss';
-
-// Imports all fontawesome core and solid icons
-// jhipster-needle-add-element-to-vendor - JHipster will add new menu items here
