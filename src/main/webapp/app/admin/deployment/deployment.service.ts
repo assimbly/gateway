ed3530d1025bdb31cf3be4c98d6d182d0c301100
root <jelles.duin@gmail.com>
2020-09-25 15:59:32 +0200

    Implemented explort configuration

--- src/main/webapp/app/admin/deployment/deployment.service.ts
+++ src/main/webapp/app/admin/deployment/deployment.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FlowService } from 'app/entities/flow';
 import { GatewayService } from 'app/entities/gateway';
+import { IGateway } from 'app/shared/model/gateway.model';
 
 @Injectable()
 export class DeploymentService {
     constructor(protected http: HttpClient, protected flowService: FlowService, protected gatewayService: GatewayService) {}
 
-    exportGatewayConfiguration() {
-        // TODO: GET CORRECT GATEWAY
-        // this.flowService.exportGatewayConfiguration(gateway);
+    exportGatewayConfiguration(gateway: IGateway) {
+        this.flowService.exportGatewayConfiguration(gateway);
     }
 }
