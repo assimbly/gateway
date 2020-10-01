d584b68ee4120e6b470f498c9f8db775e019dc09
root <jelles.duin@gmail.com>
2020-10-01 12:56:04 +0200

    Export flow list

--- src/main/webapp/app/admin/deployment/deployment.searchbyname.pipe.ts
+++
@@ -1,6 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'DeploymentSearchByName' })
-export class DeploymentSearchbyname implements PipeTransform {
-    transform(value: any, ...args: any[]): any {}
-}
