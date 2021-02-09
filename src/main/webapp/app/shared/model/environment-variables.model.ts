b050808d067e79dccfd88d68bc474e35402e69e2
AVJSJAUW <jsjauwmook@svb.nl>
2021-02-09 15:05:18 +0100

    add jasypt encryption

--- src/main/webapp/app/shared/model/environment-variables.model.ts
+++ src/main/webapp/app/shared/model/environment-variables.model.ts
@@ -2,9 +2,10 @@ export interface IEnvironmentVariables {
     id?: number;
     key?: string;
     value?: string;
+    encrypt?: boolean;
     gatewayId?: number;
 }
 
 export class EnvironmentVariables implements IEnvironmentVariables {
-    constructor(public id?: number, public key?: string, public value?: string, public gatewayId?: number) {}
+    constructor(public id?: number, public key?: string, public value?: string, public encrypt?: boolean, public gatewayId?: number) {}
 }
