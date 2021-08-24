d791dc6a86faae36a2f7c1b19237f0f761c19316
AVRMEEST <rmeester@svb.nl>
2021-08-24 16:02:32 +0200

    Add New Broker Pages (Sender/Browser)

---
+++ src/main/webapp/app/entities/broker/message.searchbymessageid.pipe.ts
@@ -0,0 +1,9 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IMessage } from 'app/shared/model/messsage.model';
+
+@Pipe({ name: 'MessageSearchByMessageId' })
+export class MessageSearchByMessageIdPipe implements PipeTransform {
+    transform(messages: IMessage[], searchText: string) {
+        return messages.filter(message => message.messageid.toLocaleLowerCase().indexOf(searchText.toLocaleLowerCase()) !== -1);
+    }
+}
