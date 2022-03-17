742e955d76f655cdf1692aa66badc16881327131
assimbly <r.meester@caesar.nl>
2022-03-17 11:03:24 +0100

    small security fix

--- src/main/java/org/assimbly/gateway/config/SecurityConfiguration.java
+++ src/main/java/org/assimbly/gateway/config/SecurityConfiguration.java
@@ -49,23 +49,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) {
-        web
-            .ignoring()
-            .antMatchers(HttpMethod.OPTIONS, "/**")
-            .antMatchers("/app/**/*.{js,html}")
-            .antMatchers("/i18n/**")
-            .antMatchers("/content/**")
-            .antMatchers("/h2-console/**")
-            .antMatchers("/swagger-ui/**")
-            .antMatchers("/test/**")
-			.antMatchers("/management/jolokia/**")
-            .antMatchers("/management/hawtio/**")
-            .antMatchers("/jolokia/**");
-    }
-
-    @Override
     public void configure(HttpSecurity http) throws Exception {
+		
+		String[] staticResources  =  {
+                "/**","/app/**/*.{js,html}","/i18n/**","/content/**","/h2-console/**","/swagger-ui/**","/test/**"
+		};
+		
         // @formatter:off
         http
             .csrf()
@@ -89,6 +78,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
             .authorizeRequests()
+			.antMatchers(HttpMethod.OPTIONS).permitAll()	
+			.antMatchers(staticResources).permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
@@ -97,7 +88,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/**").authenticated()
             .antMatchers("/websocket/**").permitAll() 
-			.antMatchers("/ws/**").permitAll()			
+			.antMatchers("/ws/**").permitAll()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/health/**").permitAll()
             .antMatchers("/management/info").permitAll()
