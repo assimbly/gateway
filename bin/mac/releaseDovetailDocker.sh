e81fb469a883526278a77e135042bcde20452a1a
skin27 <raymondmeester@gmail.com>
2022-11-09 14:16:27 +0100

    fix for Docker image with Dovetail

--- bin/mac/releaseDovetailDocker.sh
+++ bin/mac/releaseDovetailDocker.sh
@@ -3,5 +3,5 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-clear & ./../../gradlew clean bootJar jib -Djib.to.credHelper=ecr-login -Djib.to.image=/assimbly:snapshot --settings-file ./../../settings.gradle  -Papi-docs -Pprod -Pheadless -Pdovetail
+clear & ./../../gradlew clean bootJar jib -Djib.to.credHelper=ecr-login -Djib.to.image=aws_account_id.dkr.ecr.region.amazonaws.com/assimbly:snapshot --settings-file ./../../settings.gradle  -Papi-docs -Pprod -Pheadless -Pdovetail
 
