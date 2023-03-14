bef211ff60eb2473d47c86b0e4e89a954f484d2a
skin27 <raymondmeester@gmail.com>
2023-03-14 16:41:49 +0100

    Fix double entries

--- src/main/java/org/assimbly/gateway/repository/LinkRepository.java
+++ src/main/java/org/assimbly/gateway/repository/LinkRepository.java
@@ -1,8 +1,12 @@
 package org.assimbly.gateway.repository;
 
 import org.assimbly.gateway.domain.Link;
+import org.assimbly.gateway.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Spring Data  repository for the Link entity.
@@ -11,5 +15,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LinkRepository extends JpaRepository<Link, Long> {
     void deleteByStepId(Long stepId);
+
+    Optional<Set<Link>> findByName(String name);
 
 }
