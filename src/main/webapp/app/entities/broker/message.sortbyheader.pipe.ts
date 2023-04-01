030ee13ed42c846c75508dd1c5c492818a3b4a50
skin27 <raymondmeester@gmail.com>
2023-04-01 15:11:15 +0200

    fix broker frontend

--- src/main/webapp/app/entities/broker/message.sortbyheader.pipe.ts
+++ src/main/webapp/app/entities/broker/message.sortbyheader.pipe.ts
@@ -7,16 +7,21 @@ export class MessageSortByHeaderPipePipe implements PipeTransform {
     transform(headers: KeyValue<string, any>[], ascending: boolean, predicate: string) {
         let sortedHeaders: Header[] = [];
 
-        for (let i = 0; i < headers.length; i++) {
-            sortedHeaders.push(new Header(i, headers[i].key, headers[i].value, null, null, null));
-        }
-        const asc: number = ascending ? 1 : -1;
+        if(headers){
 
-        if (predicate == 'key') {
-            sortedHeaders = sortedHeaders.sort((a, b) => (a.key.toLocaleLowerCase() < b.key.toLocaleLowerCase() ? asc : asc * -1));
-        } else if (predicate == 'value') {
-            sortedHeaders = sortedHeaders.sort((a, b) => (a.value <= b.value ? asc : asc * -1));
+          for (let i = 0; i < headers.length; i++) {
+              sortedHeaders.push(new Header(i, headers[i].key, headers[i].value, null, null, null));
+          }
+          const asc: number = ascending ? 1 : -1;
+
+          if (predicate == 'key') {
+              sortedHeaders = sortedHeaders.sort((a, b) => (a.key.toLocaleLowerCase() < b.key.toLocaleLowerCase() ? asc : asc * -1));
+          } else if (predicate == 'value') {
+              sortedHeaders = sortedHeaders.sort((a, b) => (a.value <= b.value ? asc : asc * -1));
+          }
+
         }
+
         return sortedHeaders;
     }
 }
