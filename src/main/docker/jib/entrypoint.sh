d4ad9b75118960db2c3ae7d984fa39268c3765c3
skin27 <raymondmeester@gmail.com>
2023-05-16 08:12:39 +0200

    set jvm params on entrypoint

--- src/main/docker/jib/entrypoint.sh
+++ src/main/docker/jib/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
-exec java ${JAVA_OPTS} -noverify -XX:MaxRAMPercentage=80 -XX:MinRAMPercentage=50 -XX:InitialRAMPercentagle=5 -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "org.assimbly.gateway.GatewayApp"  "$@"  --application.gateway.base-directory=/data  --server.port=8088
+exec java ${JAVA_OPTS} -noverify -XX:InitialRAMPercentagle=5 -XX:MinRAMPercentage=50 -XX:MaxRAMPercentage=80 -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "org.assimbly.gateway.GatewayApp"  "$@"  --application.gateway.base-directory=/data  --server.port=8088
