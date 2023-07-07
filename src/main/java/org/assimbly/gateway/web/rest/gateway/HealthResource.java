a8b5db0e950896950d95e39a77fbacea885ba5ec
Bruno Gonçalves <brunovg@gmail.com>
2023-07-07 11:34:31 +0100

    renamed HealthCheck to Health

--- src/main/java/org/assimbly/gateway/web/rest/gateway/HealthResource.java
+++ src/main/java/org/assimbly/gateway/web/rest/gateway/HealthResource.java
@@ -3,7 +3,7 @@ package org.assimbly.gateway.web.rest.gateway;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.assimbly.dil.validation.stats.BackendResponse;
-import org.assimbly.gateway.service.HealthCheckService;
+import org.assimbly.gateway.service.HealthService;
 import org.assimbly.util.rest.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,15 +22,15 @@ import java.nio.charset.StandardCharsets;
  * REST controller for getting the {@link AuditEvent}s.
  */
 @RestController
-@RequestMapping("/health/check")
-public class HealthCheckResource {
+@RequestMapping("/health")
+public class HealthResource {
 
     protected Logger log = LoggerFactory.getLogger(getClass());
 
-    private final HealthCheckService healthCheckService;
+    private final HealthService healthService;
 
-    public HealthCheckResource(HealthCheckService healthCheckService) {
-        this.healthCheckService = healthCheckService;
+    public HealthResource(HealthService healthService) {
+        this.healthService = healthService;
     }
 
     private boolean plainResponse;
@@ -52,23 +52,23 @@ public class HealthCheckResource {
 
             MemoryUsage mem = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
 
-            backendResponse.addMemory("current", healthCheckService.convertSizeToKb(mem.getUsed()));
-            backendResponse.addMemory("max", healthCheckService.convertSizeToKb(mem.getMax()));
-            backendResponse.addMemory("committed", healthCheckService.convertSizeToKb(mem.getCommitted()));
-            backendResponse.addMemory("cached", healthCheckService.convertSizeToKb(mem.getCommitted() - mem.getUsed()));
+            backendResponse.addMemory("current", healthService.convertSizeToKb(mem.getUsed()));
+            backendResponse.addMemory("max", healthService.convertSizeToKb(mem.getMax()));
+            backendResponse.addMemory("committed", healthService.convertSizeToKb(mem.getCommitted()));
+            backendResponse.addMemory("cached", healthService.convertSizeToKb(mem.getCommitted() - mem.getUsed()));
             backendResponse.addMemory("currentUsedPercentage", (mem.getUsed() * 100 / mem.getMax()));
 
             backendResponse.addThread("threadCount", threadMXBean.getThreadCount());
             backendResponse.addThread("peakThreadCount", threadMXBean.getPeakThreadCount());
 
-            backendResponse.addJvm("openFileDescriptors", healthCheckService.invokeMethod("getOpenFileDescriptorCount"));
-            backendResponse.addJvm("maxFileDescriptors", healthCheckService.invokeMethod("getMaxFileDescriptorCount"));
+            backendResponse.addJvm("openFileDescriptors", healthService.invokeMethod("getOpenFileDescriptorCount"));
+            backendResponse.addJvm("maxFileDescriptors", healthService.invokeMethod("getMaxFileDescriptorCount"));
 
             mapper.writeValue(out, backendResponse);
-            return org.assimbly.util.rest.ResponseUtil.createSuccessResponse(integrationId, mediaType, "/health/check/jvm", out.toString(StandardCharsets.UTF_8), plainResponse);
+            return org.assimbly.util.rest.ResponseUtil.createSuccessResponse(integrationId, mediaType, "/health/jvm", out.toString(StandardCharsets.UTF_8), plainResponse);
         } catch (Exception e) {
             log.error("Get jvm failed",e);
-            return ResponseUtil.createFailureResponse(integrationId, mediaType,"/health/check/jvm",e.getMessage());
+            return ResponseUtil.createFailureResponse(integrationId, mediaType,"/health/jvm",e.getMessage());
         }
     }
 }
