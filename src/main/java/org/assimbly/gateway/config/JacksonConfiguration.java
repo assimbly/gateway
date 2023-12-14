4b4e3cb238e0d76fa8492b912b06b117b6e1b015
Raymond Meester <raymondmeester@gmail.com>
2023-12-14 23:38:36 +0100

    remove zalando problem in favor of default Spring

--- src/main/java/org/assimbly/gateway/config/JacksonConfiguration.java
+++ src/main/java/org/assimbly/gateway/config/JacksonConfiguration.java
@@ -1,6 +1,7 @@
 package org.assimbly.gateway.config;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module.Feature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ public class JacksonConfiguration {
 
     /**
      * Support for Java date and time API.
+     *
      * @return the corresponding Jackson module.
      */
     @Bean
@@ -27,11 +29,8 @@ public class JacksonConfiguration {
      * Support for Hibernate types in Jackson.
      */
     @Bean
-    public Hibernate5Module hibernate5Module() {
-        return new Hibernate5Module();
+    public Hibernate6Module hibernate6Module() {
+        return new Hibernate6Module().configure(Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS, true);
     }
 
-    /*
-     * Module for serialization/deserialization of RFC7807 Problem.
-     */
 }
