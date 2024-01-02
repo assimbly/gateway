1f7643b31fd652bda44d4105a451ba623cbfd838
Raymond Meester <raymondmeester@gmail.com>
2024-01-02 09:12:01 +0100

    upgrade dependencies

--- gradlew.bat
+++ gradlew.bat
@@ -52,7 +52,8 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
+rem set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_HOME=D:/test/java21_2/graalvm-jdk-21.0.1+12.1
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
