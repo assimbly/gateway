37aeb5b77a0986c07e58899852e1bba3f05bfdb5
Raymond Meester <raymondmeester@gmail.com>
2024-01-29 14:38:45 +0100

    restore webapp directory

---
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -0,0 +1,63 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { BrokerModule } from './broker/broker.module';
+import { CertificateModule } from './certificate/certificate.module';
+import { StepModule } from './step/step.module';
+import { LinkModule } from './link/link.module';
+import { EnvironmentVariablesModule } from './environment-variables/environment-variables.module';
+import { FlowModule } from './flow/flow.module';
+import { IntegrationModule } from './integration/integration.module';
+import { MessageModule } from './message/message.module';
+import { HeaderModule } from './header/header.module';
+import { QueueModule } from './queue/queue.module';
+import { RouteModule } from './route/route.module';
+import { ConnectionModule } from './connection/connection.module';
+import { ConnectionKeysModule } from './connection-keys/connection-keys.module';
+import { TopicModule } from './topic/topic.module';
+
+import { DeploymentService } from 'app/admin/deployment/deployment.service';
+/* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
+
+@NgModule({
+  // prettier-ignore
+  imports: [
+        BrokerModule,
+		CertificateModule,
+        StepModule,
+        LinkModule,
+		    EnvironmentVariablesModule,
+		    FlowModule,
+		    IntegrationModule,
+        MessageModule,
+        HeaderModule,
+		    QueueModule,
+        RouteModule,
+        ConnectionModule,
+        ConnectionKeysModule,
+		TopicModule,
+        /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
+    ],
+  exports: [
+        BrokerModule,
+		    CertificateModule,
+        LinkModule,
+        StepModule,
+    		EnvironmentVariablesModule,
+		    FlowModule,
+	      IntegrationModule,
+        MessageModule,
+        HeaderModule,
+	      QueueModule,
+        RouteModule,
+        ConnectionModule,
+        ConnectionKeysModule,
+	    TopicModule,
+    /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
+  ],
+  declarations: [],
+  entryComponents: [],
+  providers: [DeploymentService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class EntityRoutingModule {}
