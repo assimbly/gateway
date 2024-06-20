b3ad66ec6950a3dcf64de7988d9e2c6b996f8cd7
Bruno Gonçalves <brunovg@gmail.com>
2024-06-20 18:41:56 +0100

    cast role and status fields with the right type, on user data object

--- src/main/java/org/assimbly/gateway/web/rest/headless/AccountDBResource.java
+++ src/main/java/org/assimbly/gateway/web/rest/headless/AccountDBResource.java
@@ -59,7 +59,7 @@ public class AccountDBResource {
 
             return ResponseEntity.ok().body(token);
         } catch (Exception e) {
-            log.debug("Error to authenticate", e);
+            log.error("Error to authenticate", e);
             return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body("Invalid authentication");
