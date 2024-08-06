da750293e140348c51dea5a597ae1bbbdb941262
Bruno Gonçalves <brunovg@gmail.com>
2024-08-06 09:01:49 +0100

    using keystore password from environment variable KEYSTORE_PWD

--- src/main/webapp/app/app.constants.ts
+++ src/main/webapp/app/app.constants.ts
@@ -6,7 +6,9 @@
 declare const __DEBUG_INFO_ENABLED__: boolean;
 declare const __VERSION__: string;
 declare const __TYPE__: string;
+declare const __KEYSTORE_PWD__: string;
 
 export const VERSION = __VERSION__;
 export const DEBUG_INFO_ENABLED = __DEBUG_INFO_ENABLED__;
 export const TYPE = __TYPE__;
+export const KEYSTORE_PWD = __KEYSTORE_PWD__;
