4f17d4cbee65ddb92f753c578068d022fcb16a62
Bruno Gonçalves <brunovg@gmail.com>
2024-09-26 16:49:35 +0100

    add TenantVariable field _type

--- src/main/java/org/assimbly/gateway/variables/domain/TenantVariable.java
+++ src/main/java/org/assimbly/gateway/variables/domain/TenantVariable.java
@@ -10,12 +10,19 @@ import java.util.Optional;
 public class TenantVariable {
 
     public static final String ID_FIELD = "_id";
+    public static final String TYPE_FIELD = "_type";
     public static final String NAME_FIELD = "name";
     public static final String CREATED_AT_FIELD = "createdAt";
     public static final String CREATED_BY_FIELD = "createdBy";
     public static final String VALUES_FIELD = "values";
 
+    public enum TenantVarType {
+        TenantVariable,
+        StaticTenantVariable;
+    }
+
     private ObjectId _id;
+    private String _type;
     private String name;
     private long createdAt;
     private String createdBy;
@@ -24,11 +31,20 @@ public class TenantVariable {
 
     public TenantVariable(){
         this._id = new ObjectId();
+        this._type = TenantVarType.TenantVariable.name();
         this.values = new ArrayList<>();
     }
 
     public TenantVariable(String name){
         this._id = new ObjectId();
+        this._type = TenantVarType.TenantVariable.name();
+        this.name = name;
+        this.values = new ArrayList<>();
+    }
+
+    public TenantVariable(String name, TenantVarType tenantVarType){
+        this._id = new ObjectId();
+        this._type = tenantVarType.name();
         this.name = name;
         this.values = new ArrayList<>();
     }
@@ -50,6 +66,9 @@ public class TenantVariable {
     public static TenantVariable fromDocument(Document document) {
         TenantVariable tenantVariable = new TenantVariable();
         tenantVariable.set_id(document.getObjectId(ID_FIELD));
+        if(document.getString(TYPE_FIELD) != null) {
+            tenantVariable.set_type(document.getString(TYPE_FIELD));
+        }
         tenantVariable.setName(document.getString(NAME_FIELD));
         if(document.getLong(CREATED_AT_FIELD) != null) {
             tenantVariable.setCreatedAt(document.getLong(CREATED_AT_FIELD));
@@ -77,6 +96,7 @@ public class TenantVariable {
     public Document toDocument() {
         Document document = new Document();
         document.append(ID_FIELD, this.get_id());
+        document.append(TYPE_FIELD, this.get_type());
         document.append(NAME_FIELD, this.getName());
         document.append(CREATED_AT_FIELD, this.getCreatedAt());
         document.append(CREATED_BY_FIELD, this.getCreatedBy());
@@ -104,6 +124,14 @@ public class TenantVariable {
 
     public void set_id(ObjectId _id) {
         this._id = _id;
+    }
+
+    public String get_type() {
+        return _type;
+    }
+
+    public void set_type(String _type) {
+        this._type = _type;
     }
 
     public String getName() {
