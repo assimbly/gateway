1051a534422406a4be499cc8c1e6bd170ff8126b
Bruno Gonçalves <brunovg@gmail.com>
2025-05-29 16:05:11 +0100

    Description correction

--- src/main/java/org/assimbly/gateway/web/rest/headless/EmailResource.java
+++ src/main/java/org/assimbly/gateway/web/rest/headless/EmailResource.java
@@ -25,7 +25,7 @@ import java.io.InputStream;
 import java.util.Collections;
 
 /**
- * REST controller for managing oauth2 (Open Authorization v2).
+ * REST controller to interact with Google Mail service using oauth2 (m2m)
  */
 @RestController
 @RequestMapping("/api/email")
@@ -40,8 +40,8 @@ public class EmailResource {
     }
 
     /**
-     * GET  /info : requests oauth2 access token info
-     * @return Map token information
+     * POST  /send : send email
+     * @return response status
      */
     @PostMapping(
         path = "/send",
