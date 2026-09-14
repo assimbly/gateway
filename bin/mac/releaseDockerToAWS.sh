f5cdf1dc4aafbdf287ee149857dd4d4f2dc95f44
Bruno Gonçalves <brunovg@gmail.com>
2026-09-14 16:31:43 +0100

    release images for both platforms, linux/arm64 and linux/amd64

--- bin/mac/releaseDockerToAWS.sh
+++ bin/mac/releaseDockerToAWS.sh
@@ -4,9 +4,33 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 property_file="../../gradle.properties"
 
+# backend
 sed -i .bak "s/type=.*/type=HEADLESS/" $property_file
-clear & ./../../gradlew clean bootJar jib -Djib.to.credHelper=ecr-login -Djib.to.image=902987087860.dkr.ecr.eu-west-1.amazonaws.com/gateway-headless:next --project-dir ./../../ -Papi-docs -Pprod -Pfull -Pheadless
+# arm
+clear & ./../../gradlew clean bootJar jib \
+    -Djib.to.credHelper=ecr-login -Djib.to.image=902987087860.dkr.ecr.eu-west-1.amazonaws.com/backend-arm:next \
+    -Djib.from.platforms=linux/arm64 \
+    --project-dir ./../../ \
+    -Papi-docs -Pprod -Pfull -Pheadless
+# x86
+clear & ./../../gradlew clean bootJar jib \
+    -Djib.to.credHelper=ecr-login -Djib.to.image=902987087860.dkr.ecr.eu-west-1.amazonaws.com/backend:next \
+    -Djib.from.platforms=linux/amd64 \
+    --project-dir ./../../ \
+    -Papi-docs -Pprod -Pfull -Pheadless
 
+# broker
 sed -i .bak "s/type=.*/type=BROKER/" $property_file
-clear & ./../../gradlew clean bootJar jib -Djib.to.credHelper=ecr-login -Djib.to.image=902987087860.dkr.ecr.eu-west-1.amazonaws.com/gateway-broker:next --project-dir ./../../ -Papi-docs -Pprod -Pbroker
+# arm
+clear & ./../../gradlew clean bootJar jib \
+    -Djib.to.credHelper=ecr-login -Djib.to.image=902987087860.dkr.ecr.eu-west-1.amazonaws.com/broker-arm:next \
+    -Djib.from.platforms=linux/arm64 \
+    --project-dir ./../../ \
+    -Papi-docs -Pprod -Pbroker
+# x86
+clear & ./../../gradlew clean bootJar jib \
+    -Djib.to.credHelper=ecr-login -Djib.to.image=902987087860.dkr.ecr.eu-west-1.amazonaws.com/broker:next \
+    -Djib.from.platforms=linux/amd64 \
+    --project-dir ./../../ \
+    -Papi-docs -Pprod -Pbroker
 
